bf4e27e68f2f80e04541829480e89bb85d143d5c
Bhargav Bhat <bhargavbhat@gmail.com>
2016-12-27 16:36:30 +0530

    Limit results to just the top post

--- curl_wrapper.h
+++ curl_wrapper.h
@@ -3,7 +3,7 @@
 #include <string>
 #include <curl/curl.h>
 
-const std::string uri = "https://www.reddit.com/r/todayilearned/top/.json";
+const std::string uri = "https://www.reddit.com/r/todayilearned/top/.json?count=1";
 const std::string ua = "clitil/1.0";
 
 class CurlWrapper
